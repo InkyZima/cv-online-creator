a9bcf4dce69da2242b0585df40a88e34310084ea
Inky Zima <inky.zima@gmail.com>
2018-12-28 18:28:13 +0100

    deploy

---
+++ precache-manifest.969835911a35ace96fe5c3704b1df049.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.svg"
+  },
+  {
+    "revision": "ffec432016d09b9ac44d9bd71e620611",
+    "url": "/img/hero.jpg"
+  },
+  {
+    "revision": "f736edc020068094021cd05413307d11",
+    "url": "/img/section.jpg"
+  },
+  {
+    "revision": "9a03ffaa6278d0f5c6e3",
+    "url": "/js/chunk-vendors.050dde9e.js"
+  },
+  {
+    "revision": "506a34026a719d3dd667f81d6a350263",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7b50ceed696bdc5ed17fc534adbfe3c",
+    "url": "/sampleCVs/template.json"
+  },
+  {
+    "revision": "bb4742623368cf8204a3787db6188a56",
+    "url": "/sampleCVs/ThomasEdison.json"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.png"
+  },
+  {
+    "revision": "e026343bec41ecc9c9d6",
+    "url": "/js/app.ee182bad.js"
+  },
+  {
+    "revision": "b5d0c929566a5886a1e3cbbb4f303097",
+    "url": "/img/plane.jpg"
+  },
+  {
+    "revision": "c2a587dfec88c9c9bb64e8d3510a2b34",
+    "url": "/sampleCVs/ThomasEdison_de.json"
+  },
+  {
+    "revision": "5d967ec4d908bc09a63d13a467113e20",
+    "url": "/img/vuetify.png"
+  },
+  {
+    "revision": "6ab2e6dd5ae71df7d712cc8b63e6b754",
+    "url": "/sampleCVs/ThomasEdison_old.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "ffec432016d09b9ac44d9bd71e620611",
+    "url": "/img/hero.1.jpg"
+  },
+  {
+    "revision": "9a03ffaa6278d0f5c6e3",
+    "url": "/css/chunk-vendors.8dd90e06.css"
+  },
+  {
+    "revision": "e026343bec41ecc9c9d6",
+    "url": "/css/app.9780522c.css"
+  }
+];
